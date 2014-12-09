c645d3dee76b6def43573d520c82f395472cef42
Matt Olson <matt@mlolson.local>
2014-12-09 09:26:11 -0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//jQuery to alter the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
